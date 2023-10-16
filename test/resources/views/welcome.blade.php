c9d7e2856c5f65a870c31a22f9f33ce850d1c6e4
Adil Chergui <124139176+cherguiadil@users.noreply.github.com>
2023-10-16 07:52:02 +0100

    Update welcome.blade.php

--- test/resources/views/welcome.blade.php
+++ test/resources/views/welcome.blade.php
@@ -2,9 +2,8 @@
 <html>
 
 <head>
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.css" >
+
 
 </head>
 
@@ -51,6 +50,10 @@
     </div>
 
 
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <script src='https://unpkg.com/popper.js'></script>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             function render() {
@@ -90,6 +93,8 @@
 
         });
     </script>
+
+
 </body>
 
 </html>
