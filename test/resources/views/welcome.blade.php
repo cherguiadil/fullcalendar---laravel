9685fab60dfb5b68aeee0f879892499509a8fc22
Adil Chergui <124139176+cherguiadil@users.noreply.github.com>
2023-11-07 09:57:15 +0100

    Update welcome.blade.php

--- test/resources/views/welcome.blade.php
+++ test/resources/views/welcome.blade.php
@@ -1,15 +1,52 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/5.10.1/main.css" rel="stylesheet"> --}}
-    <link rel='stylesheet' href='https://fullcalendar.io/releases/core/4.0.1/main.min.css'>
-    <link rel='stylesheet' href='https://fullcalendar.io/releases/timeline/4.0.1/main.min.css'>
-    <link rel='stylesheet' href='https://fullcalendar.io/releases/resource-timeline/4.0.1/main.min.css'>
-</head>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.min.js"></script>
 
+</head>
 <body>
-    <div id='calendar'></div>
+    <div id="calendar"></div>
+
+<!-- Add an "Add Event" button -->
+<button id="addEventButton" class="btn btn-primary">Add Event</button>
+
+<!-- Create a modal for adding events -->
+<div class="modal fade" id="addEventModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Event</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="eventForm">
+                    @csrf
+                    <div class="form-group">
+                        <label for="title">Event Title</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="form-group">
+                        <label for="start">Start Date and Time</label>
+                        <input type="datetime-local" class="form-control" id="start" name="start">
+                    </div>
+                    <div class="form-group">
+                        <label for="end">End Date and Time</label>
+                        <input type="datetime-local" class="form-control" id="end" name="end">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="saveEventButton">Save Event</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
     <script src='https://fullcalendar.io/releases/core/4.0.1/main.min.js'></script>
@@ -18,28 +55,46 @@
     <script src='https://fullcalendar.io/releases/resource-common/4.0.1/main.min.js'></script>
     <script src='https://fullcalendar.io/releases/resource-timeline/4.0.1/main.min.js'></script>
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
+
+        function render() {
             var calendarEl = document.getElementById('calendar');
-
             var calendar = new FullCalendar.Calendar(calendarEl, {
-                plugins: ['resourceTimeline'],
-                timeZone: 'UTC',
-                initialView: 'resourceTimelineDay',
-                aspectRatio: 2.5,
-                headerToolbar: {
-                    left: 'prev,next',
-                    center: 'title',
-                    right: 'resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth'
-                },
-                editable: true,
-                resourceLabelText: 'Rooms',
-                resources: 'https://fullcalendar.io/demo-resources.json?with-nesting&with-colors',
-                events: 'https://fullcalendar.io/demo-events.json?single-day&for-resource-timeline'
+                initialView: 'dayGridMonth',
+                events: "{{ route('calendar.events') }}",
             });
-
             calendar.render();
+        }
+        document.addEventListener('DOMContentLoaded', function() {
+            render()
         });
+
+
+        document.getElementById('saveEventButton').addEventListener('click', function() {
+
+    // Get the form data
+    var form = document.getElementById('eventForm');
+    var formData = new FormData(form);
+
+    // Send a POST request to create the event
+    fetch("{{ route('calendar.store') }}", {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.message) {
+            render();
+        } else {
+            alert('Event creation failed. Please check your input.');
+        }
+    })
+    .catch(error => {
+        alert(error);
+    });
+});
+
+
     </script>
 </body>
-
 </html>
